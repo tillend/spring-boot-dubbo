103767a81a848b8a04f75b9c584d2b42be282baa
Lin <weihaolin2008@qq.com>
2018-07-19 23:05:46 +0800

    集成validator参数验证

--- spring-boot-dubbo-provider/src/main/java/org/spring/boot/dubbo/provider/DemoServiceImpl.java
+++ spring-boot-dubbo-provider/src/main/java/org/spring/boot/dubbo/provider/DemoServiceImpl.java
@@ -1,16 +1,19 @@
 package org.spring.boot.dubbo.provider;
 
+import javax.validation.Valid;
+
 import org.spring.boot.dubbo.api.DemoService;
+import org.spring.boot.dubbo.api.model.Word;
 
 import com.alibaba.dubbo.common.utils.StringUtils;
 import com.alibaba.dubbo.config.annotation.Service;
 
-@Service(version = "${demo.service.version}")
+@Service(version = "${demo.service.version}", validation = "true")
 public class DemoServiceImpl implements DemoService {
 
-	@Override
-	public String saySomething(String word) {
-		return StringUtils.isNotEmpty(word) ? word : "Say you won't let go";
-	}
+    @Override
+    public String saySomething(@Valid Word word) {
+        return StringUtils.isNotEmpty(word.getWord()) ? word.getWord() : "Say you won't let go";
+    }
 
 }
