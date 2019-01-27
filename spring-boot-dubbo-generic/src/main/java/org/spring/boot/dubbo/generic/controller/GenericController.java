ff0745592b18a0b1fd415c59eb7e0ebf3eff1d92
Lin <weihaolin2008@qq.com>
2019-01-27 17:56:31 +0800

    增加代码可读性

--- spring-boot-dubbo-generic/src/main/java/org/spring/boot/dubbo/generic/controller/GenericController.java
+++ spring-boot-dubbo-generic/src/main/java/org/spring/boot/dubbo/generic/controller/GenericController.java
@@ -1,22 +1,19 @@
 package org.spring.boot.dubbo.generic.controller;
 
-import org.spring.boot.dubbo.generic.model.GenericReqModel;
-import org.spring.boot.dubbo.generic.utils.DubboUtils;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.alibaba.dubbo.rpc.service.GenericService;
 import com.alibaba.fastjson.JSON;
+import org.spring.boot.dubbo.generic.model.GenericReqModel;
+import org.spring.boot.dubbo.generic.utils.DubboUtils;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/")
 public class GenericController {
 
-    @RequestMapping(value = "/generic", consumes = "application/json; charset=UTF-8",
-        method = {RequestMethod.POST, RequestMethod.GET})
+    @RequestMapping(value = "/generic", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE,
+            produces = MediaType.APPLICATION_JSON_UTF8_VALUE,
+            method = {RequestMethod.POST, RequestMethod.GET})
     @ResponseBody
     public Object call(@ModelAttribute("reqModel") GenericReqModel reqModel) throws ClassNotFoundException {
         Object result = null;
@@ -29,12 +26,11 @@ public class GenericController {
 
         String[] parameterTypes = DubboUtils.getMethodParamType(reqModel.getService(), reqModel.getMethod());
 
-        Object[] args = JSON.parseArray(reqModel.getParamValues()).toArray(new Object[] {});
+        Object[] args = JSON.parseArray(reqModel.getParamValues()).toArray(new Object[]{});
 
         // 3.调用接口
-        result = JSON.toJSONString(service.$invoke(method, parameterTypes, args));
+        return JSON.toJSONString(service.$invoke(method, parameterTypes, args));
 
-        return result;
     }
 
 }
